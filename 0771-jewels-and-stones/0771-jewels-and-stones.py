c0e37cd60d6f8178cf7457079cdc899201ff3059
Talha Abid <talhabid420@gmail.com>
2026-06-10 03:05:41 -0700

    Time: 2 ms (17.46%), Space: 12.4 MB (18.73%) - LeetHub

--- 0771-jewels-and-stones/0771-jewels-and-stones.py
+++ 0771-jewels-and-stones/0771-jewels-and-stones.py
@@ -1,9 +1,13 @@
 class Solution(object):
     def numJewelsInStones(self, jewels, stones):
         count = 0
-        for i in range(len(jewels)):
-            for j in range(len(stones)):
-                if jewels[i] == stones[j]:
-                    count += 1
-        return count
+        hash_set = set()
         
+        for i in range(len(jewels)):
+            hash_set.add(jewels[i])
+
+        for j in range(len(stones)):
+            if stones[j] in hash_set:
+                count += 1
+        
+        return count
