fce5819ce7d4776a9116f91a0d1a9e1f501fe96b
Talha Abid <talhabid420@gmail.com>
2026-06-12 22:45:48 -0700

    Time: 0 ms (100%), Space: 13.5 MB (11.3%) - LeetHub

--- 0412-fizz-buzz/0412-fizz-buzz.py
+++ 0412-fizz-buzz/0412-fizz-buzz.py
@@ -3,10 +3,9 @@ class Solution(object):
         result = []
         for i in range(1, n+1):
             string = ""
-            if i % 3 == 0:
-                string += "Fizz"
-            if i % 5 == 0:
-                string += "Buzz"
+
+            if i % 3 == 0:  string += "Fizz"
+            if i % 5 == 0:  string += "Buzz"
 
             result.append(string or str(i))
 
