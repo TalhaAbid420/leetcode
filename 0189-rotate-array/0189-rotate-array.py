0ab530ba7d0496d9f1d7b2b283a37f52d9dca218
Talha Abid <talhabid420@gmail.com>
2026-06-16 21:52:30 -0700

    Time: 8 ms (38.11%), Space: 23.5 MB (15.71%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.py
+++ 0189-rotate-array/0189-rotate-array.py
@@ -1,8 +1,20 @@
-class Solution:
-    def rotate(self, nums: List[int], k: int) -> None:
+class Solution(object):
+    def rotate(self, nums, k):
         """
-        Do not return anything, modify nums in-place instead.
+        :type nums: List[int]
+        :type k: int
+        :rtype: None Do not return anything, modify nums in-place instead.
         """
-        k = k % len(nums)
+        n = len(nums)
+        k %= n
 
-        nums[:] = nums[-k:] + nums[:-k]
+        def reverse(start, end):
+            while start < end:
+                nums[start], nums[end] = nums[end], nums[start]
+                start += 1
+                end -= 1
+
+        reverse(0, n-1)
+        reverse(0, k-1)
+        reverse(k, n-1)
+        
