c4343fa4f8837ddce0c238c92e29b36db0a9c27d
Talha Abid <talhabid420@gmail.com>
2026-06-17 21:41:32 -0700

    Time: 0 ms (100%), Space: 12.4 MB (15.5%) - LeetHub

--- 0796-rotate-string/0796-rotate-string.py
+++ 0796-rotate-string/0796-rotate-string.py
@@ -4,18 +4,4 @@ class Solution(object):
         if len(s) != len(goal):
             return False
 
-        s_list = list(s)
-
-        for _ in range(len(s)):
-            
-            if "".join(s_list) == goal:
-                return True
-
-            first_char = s_list.pop(0)
-
-            s_list.append(first_char)
-
-        return False
-
-
-        
+        return goal in (s+s)
