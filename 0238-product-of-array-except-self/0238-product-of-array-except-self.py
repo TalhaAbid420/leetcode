3fbe2d8919da0190212dd62b3c1e7787e67cc423
Talha Abid <talhabid420@gmail.com>
2026-07-23 19:43:07 +0500

    Time: 19 ms (74.92%), Space: 25.5 MB (55.02%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.py
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.py
@@ -3,17 +3,17 @@ class Solution:
 
         n = len(nums)
 
-        prefix = [1] * n
-        suffix = [1] * n
         answer = [1] * n
 
         for i in range(1, n):
-            prefix[i] = prefix[i - 1] * nums[i - 1]
+            answer[i] = answer[i-1] * nums[i-1]
+        
+        suffix = 1
 
-        for i in range(n - 2, -1, -1):
-            suffix[i] = suffix[i + 1] * nums[i + 1]
+        for i in range(n-1, -1, -1):
 
-        for i in range(n):
-            answer[i] = prefix[i] * suffix[i]
+            answer[i] *= suffix
+
+            suffix *= nums[i]
 
         return answer
